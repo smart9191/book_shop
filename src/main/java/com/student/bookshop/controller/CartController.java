b46530655b6a19177b3badb129ede0227260b3dd
smart9191 <galaxsysmart@gmail.com>
2023-01-22 23:56:48 +0500

    bookshop:: change book title

--- src/main/java/com/student/bookshop/controller/CartController.java
+++ src/main/java/com/student/bookshop/controller/CartController.java
@@ -9,6 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class CartController {
 
@@ -16,22 +21,32 @@ public class CartController {
     private BookService bookService;
 
     @GetMapping("/addToCart/{id}")
-    public String assToCart(@PathVariable Long id){
-
-        GlobalData.cart.add(bookService.findByIdUpdate(id).get());
+    public String assToCart(@PathVariable Long id, HttpServletRequest request){
+        List<Book> bookList = new ArrayList<>();
+        bookList= (List<Book>) request.getSession().getAttribute("NOTES_SESSION");
+        if(bookList == null){
+            bookList = new ArrayList<>();
+            request.getSession().setAttribute("NOTES_SESSION", bookList);
+        }
+        bookList.add(bookService.findByIdUpdate(id).get());
+        request.getSession().setAttribute("NOTES_SESSION", bookList);
         return "redirect:/shop";
     }
     @GetMapping("/cart")
-    public String carGet(Model model){
-        model.addAttribute("cartCount",GlobalData.cart.size());
-        model.addAttribute("total",GlobalData.cart.stream().mapToDouble(Book::getPrice).sum());
-        model.addAttribute("cart",GlobalData.cart);
+    public String carGet(Model model, HttpSession session){
+        List<Book> bookList = new ArrayList<>();
+        bookList = (List<Book>) session.getAttribute("NOTES_SESSION");
+        model.addAttribute("notesSession", bookList!=null? bookList:new ArrayList<>());
+        model.addAttribute("cartCount",bookList.size());
+        model.addAttribute("total",bookList.stream().mapToDouble(Book::getPrice).sum());
+        model.addAttribute("cart",bookList);
         return "cart";
     }
 
     @GetMapping("/cart/removeItem/{index}")
-    public String cartItemRemove(@PathVariable Integer index){
-        GlobalData.cart.remove(index);
+    public String cartItemRemove(@PathVariable Integer index,HttpSession session){
+        List<Book> bookList = (List<Book>) session.getAttribute("NOTES_SESSION");
+        bookList.remove(index);
         return "redirect:/cart";
     }
 
--- src/main/java/com/student/bookshop/controller/MainController.java
+++ src/main/java/com/student/bookshop/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.student.bookshop.controller;
 
 import com.student.bookshop.global.GlobalData;
+import com.student.bookshop.model.Book;
 import com.student.bookshop.repository.BookRepo;
 import com.student.bookshop.service.BookCategoryService;
 import com.student.bookshop.service.BookService;
@@ -9,6 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -36,25 +41,30 @@ public class MainController {
         return "index";
     }
     @GetMapping("/shop")
-    public String shop(Model model){
+    public String shop(Model model, HttpSession session){
+        List<Book> bookList = new ArrayList<>();
+        bookList = (List<Book>) session.getAttribute("NOTES_SESSION");
         model.addAttribute("categories",categoryService.findAllBookCategories());
         model.addAttribute("books",bookService.findAllBooks());
-        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("cartCount", bookList == null ? 0 : bookList.size());
         return "shop";
     }
     @GetMapping("/shop/category/{id}")
-    public String shopByCategory(@PathVariable Long id, Model model){
+    public String shopByCategory(@PathVariable Long id, Model model,HttpSession session){
+        List<Book> bookList = new ArrayList<>();
+        bookList = (List<Book>) session.getAttribute("NOTES_SESSION");
         model.addAttribute("categories",categoryService.findAllBookCategories());
         model.addAttribute("books",bookRepo.findAllByCategories_Id(id));
-        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("cartCount", bookList == null ? 0 : bookList.size());
         return "shop";
     }
 
     @GetMapping("/shop/viewbook/{id}")
-    public String viewProduct(@PathVariable Long id,  Model model){
-
+    public String viewProduct(@PathVariable Long id,  Model model,HttpSession session){
+        List<Book> bookList = new ArrayList<>();
+        bookList = (List<Book>) session.getAttribute("NOTES_SESSION");
         model.addAttribute("books", bookService.findByIdUpdate(id).get());
-        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("cartCount", bookList == null ? 0 : bookList.size());
         return "viewBook";
     }
 
